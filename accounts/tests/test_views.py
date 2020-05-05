73ae3c966c49d53f479a5d2d0b71fcc399d79d7c
mattfreire <freire1996@gmail.com>
2020-05-05 11:48:36 +0200

    Fixtures

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -7,12 +7,16 @@ from accounts.tests.common.mixins import ColumnTestDataMixin
 User = get_user_model()
 
 
-class ColumnListViewTest(ColumnTestDataMixin, TestCase):
+class ColumnListViewTest(TestCase):
+    fixtures = [
+        'users.json',
+        'columns.json'
+    ]
 
     def test_get_column_list(self):
         response = self.client.get(reverse("columns:column-list"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Test column")
+        self.assertContains(response, "a test column")
         self.assertContains(response, "Column list")
 
 
