41c2ef8be8f320910124bfd00aec92b6b7cac8eb
mattfreire <freire1996@gmail.com>
2020-05-05 12:37:01 +0200

    Request factory

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase, RequestFactory
 from django.shortcuts import reverse
-from accounts.models import Column
+from accounts.models import Column, Subscription
 from accounts.tests.common.mixins import ColumnTestDataMixin
+from accounts import views
 
 User = get_user_model()
 
@@ -18,6 +20,32 @@ class ColumnListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "a test column")
         self.assertContains(response, "Column list")
+
+
+class ColumnFeedViewTest(TestCase):
+    fixtures = [
+        'users.json',
+        'columns.json',
+        'subscriptions.json'
+    ]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.reader = User.objects.get(email="reader@jd.com")
+
+    def test_get_column_feed(self):
+        request = self.factory.get(reverse("columns:feed"))
+        request.user = self.reader
+
+        # request.user = AnonymousUser()
+        response = views.ColumnFeedView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        column_names = list(request.user.user_subscriptions.all().values_list(
+            'column__name', flat=True
+        ))
+
+        for name in column_names:
+            self.assertContains(response, name)
 
 
 # class DemoTest(TestCase):
